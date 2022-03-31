afa9375ab2706a0fda8ac35637706cbd464f473e
yeung027 <shinghei.yeung@gmail.com>
2022-03-31 17:40:19 +0800

    too confuse about image dowload, it work, but not save to camera roll in ios

--- components/generator/canvasDrawer.tsx
+++ components/generator/canvasDrawer.tsx
@@ -6,10 +6,10 @@ type MyProps = {
 };
 
 type MyStates = {
-
+  imgSrc:string
 };
 
-interface CanvasDrawer {
+interface CanvasDrawer  {
   parent: any
 }
 
@@ -21,23 +21,67 @@ class CanvasDrawer extends Component<MyProps, MyStates>
     this.parent = props.parent;
 
     this.state = {
-      
+      imgSrc: '/generator/will_smith_punching/raw.png'
     }//END state
     
+    this.mergeComplete = this.mergeComplete.bind(this);
+
   }//END constructor
+
+  mergeComplete(b64:any, imgEle: any)
+  {
+    
+    if(imgEle)
+    {
+      imgEle.src = b64;
+      //console.log(b64);
+      let sharedata:ShareData = b64;
+      var binaryData = [];
+      binaryData.push(b64);
+      let blob = new Blob(binaryData, {type: "image/png"});
+
+      const link = document.createElement("a");
+  link.href = b64;
+  link.download = 'okok.png';
+  alert(navigator.share);
+      console.log(navigator.share);
+      let ok2:any= navigator.share;
+      let url = document.location.href;
+      url = b64;
+      if(ok2)
+      {
+        alert('dsadsadsd');
+        navigator.share({url});
+      }
+
+  //link.click();
+
+
+
+//fileDownload(b64, 'filename.png');
+    }
+  }//END mergeComplete
 
   componentDidMount()
   {
-    console.log(document.querySelector('#canvasIMG'));
-    let imgEle: any  = document.querySelector('img');
+    var self = this;
+    let imgEle: any  = document.querySelector('#canvasIMG');
     if(imgEle)
+    {
+      console.log('try');
       mergeImages(['/generator/will_smith_punching/raw.png', '/generator/will_smith_punching/happy.png'])
-        .then(b64 => imgEle.src = b64);
+        .then(function (b64) 
+        {
+          self.mergeComplete(b64, imgEle);
+        });
+    }
   }//END componentDidMount
 
   render() 
   {
-      return null;
+      return  <>
+                <a href={this.state.imgSrc}>okok</a>
+              </>;
   }
 
 }//END class CanvasDrawer

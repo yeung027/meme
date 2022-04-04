2f1bd0d969db81a0fb8b45feb0e220aa7835dd80
yeung027 <shinghei.yeung@gmail.com>
2022-04-04 16:04:30 +0800

    upload img ui in button control

--- components/generator/bottomControl/uploadImg.tsx
+++ components/generator/bottomControl/uploadImg.tsx
@@ -1,7 +1,9 @@
 import React,{Component} from 'react';
 import styles from '../../../styles/generator/bottomControl/uploadImg/desktop.module.css'
 import mobileStyles from '../../../styles/generator/bottomControl/uploadImg/mobile.module.css'
-
+import utilStyles from '../../../styles/generator/bottomControl/util.module.css'
+import NotificationImportantIcon from '@material-ui/icons/NotificationImportant';
+import AddIcon from '@material-ui/icons/Add';
 
 type MyProps = {
     parent:any
@@ -35,7 +37,21 @@ class ButtonControlUploadGUI extends Component<MyProps, MyStates>
     let containerClass    = this.parent.parent.state.isMobile? mobileStyles.container : styles.container;
 
     return  <div className={containerClass}>
-              dsdsad
+              <div className={this.parent.parent.state.isMobile? mobileStyles.header : styles.header}>
+                <div className={this.parent.parent.state.isMobile? mobileStyles.title : styles.title}>
+                  <NotificationImportantIcon className={this.parent.parent.state.isMobile? mobileStyles.titleIcon : styles.titleIcon} />
+                  <span>Add image & merge</span>
+                </div>
+                <div className={this.parent.parent.state.isMobile? mobileStyles.header_r : styles.header_r}>
+                  <span className={this.parent.parent.state.isMobile? mobileStyles.textBtn : styles.textBtn}>skip</span>
+                </div>
+              </div>
+              <div className={this.parent.parent.state.isMobile? mobileStyles.main : styles.main}>
+                <div className={utilStyles.iconPurpleBtn_L}>
+                  <AddIcon className={utilStyles.icon} />
+                  <span className={utilStyles.span}>Upload</span>
+                </div>
+              </div>
             </div>
 
   }

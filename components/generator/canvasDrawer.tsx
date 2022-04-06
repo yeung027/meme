170cd99f0eeffbe5db10161bd202bbf63ef8ceff
yeung027 <shinghei.yeung@gmail.com>
2022-04-06 14:04:04 +0800

    refactor image merge and resize #2

--- components/generator/canvasDrawer.tsx
+++ components/generator/canvasDrawer.tsx
@@ -33,8 +33,11 @@ class CanvasDrawer extends Component<MyProps, MyStates>
     this.imageTextRef = React.createRef();
 
     this.mergeImg           = this.mergeImg.bind(this);
-    this.getRawImgSize      = this.getRawImgSize.bind(this);
     this.resizeIMG          = this.resizeIMG.bind(this);
+    this.getRawImgSize      = this.getRawImgSize.bind(this);
+    this.getb64ImgSize      = this.getb64ImgSize.bind(this);
+    this.getResizeRate_compareWithRaw          = this.getResizeRate_compareWithRaw.bind(this);
+    this.b64ToImgFile       = this.b64ToImgFile.bind(this);
     
   }//END constructor
 
@@ -50,24 +53,28 @@ class CanvasDrawer extends Component<MyProps, MyStates>
     //console.log(b64);
 
     let rawImageSize:any  = await this.getRawImgSize();
-    if(!rawImageSize)
+    let b64ImageSize:any  = await this.getb64ImgSize(b64.data_url);
+    if(!rawImageSize || !b64ImageSize)
     {
       console.error('Cannot get image size!');
       return false;
     }
 
-    //console.log(rawImageSize);
-    let resizedIMG:any
+    let resizedIMG:any;
+    let resizeRate:number = 0.5;
+    let finally_rate:any  = await this.getResizeRate_compareWithRaw(b64.data_url, resizeRate);
+    //console.log(b64ImageSize);
+    
+
     try
     {
-      resizedIMG  = await this.resizeIMG(b64, rawImageSize[0] * 0.2, rawImageSize[1] * 0.2);
+      resizedIMG  = await this.resizeIMG(b64, b64ImageSize[0] * finally_rate, b64ImageSize[1] * finally_rate).catch();
     }
     catch(error)
     {
       console.error(error);
       return;
     }
-    //console.log(typeof(resizedIMG));
 
     let resizedIMG_URL = URL.createObjectURL(resizedIMG);
 
@@ -88,6 +95,31 @@ class CanvasDrawer extends Component<MyProps, MyStates>
 
   }//END mergeImg
 
+  async getResizeRate_compareWithRaw(b64:any, rate:number)
+  {
+    let rawImageSize:any  = await this.getRawImgSize();
+    //console.log(b64ImageSize);
+    if(!rawImageSize)
+    {
+      console.error('Cannot get image size!');
+      return false;
+    }
+    let w:number = rawImageSize[0]
+    , h:number = rawImageSize[1];
+    
+    let b64Size:any = await this.getb64ImgSize(b64);
+    let b64_w = b64Size[0], b64_h = b64Size[1];
+    let w_rate  = b64_w / w;
+    let h_rate  = b64_h / h;
+    let smaller_rate:number  = w_rate > h_rate ? h_rate : w_rate;
+    let x_15:number = Number((Math.abs(smaller_rate) * 100).toPrecision(15));
+    let rounded_smaller_rate = Math.round(x_15) / 100 * Math.sign(smaller_rate);
+    return rate * (1 / rounded_smaller_rate);
+
+    
+
+  }//END getResizeRate_compareWithRaw
+
   resizeIMG(b64:any, w:number, h:number)
   {
     return new Promise((resolve, reject) => 
@@ -101,14 +133,38 @@ class CanvasDrawer extends Component<MyProps, MyStates>
     })
   }//END resizeIMG
 
+  b64ToImgFile(b64:any)
+  {
+    return new Promise((resolve, reject) => 
+    {
+      var image = new Image();
+      image.onload = function() {
+        resolve(image);
+      };
+
+      image.onerror = function() {
+        console.error('Error occurred while converting b64 to file');
+        reject('Error occurred while converting b64 to file');
+      };
+
+      image.src = b64;
+    });
+  }//END b64ToImgFile
+
+  getb64ImgSize(b64:any)
+  {
+    return new Promise<number[]>((resolve, reject) => {
+      let img = new Image();
+      img.onload = () => resolve([img.width, img.height])
+      img.onerror = () => reject('Error occurred while get b64 Image Size');
+      img.src = b64;
+      //resolve(this.b64ToImgFile(b64));
+    })
+  }//END getb64ImgSize
+
   getRawImgSize()
   {
-    return new Promise((resolve, reject) => {
-      let img = new Image()
-      img.onload = () => resolve([img.width, img.height])
-      img.onerror = reject
-      img.src = this.rawImgSrc;
-    })
+    return this.getb64ImgSize(this.rawImgSrc);
   }//END getRawImgSize
 
   render() 

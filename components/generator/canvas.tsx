d8015a3e850c22519c528ec9f1b7b8a52238fb8b
yeung027 <shinghei.yeung@gmail.com>
2022-04-06 16:55:46 +0800

    back home , lol

--- components/generator/canvas.tsx
+++ components/generator/canvas.tsx
@@ -1,7 +1,9 @@
-import React,{Component} from 'react';
+import React,{Component, useState} from 'react';
 import styles from '../../styles/generator/canvas/desktop.module.css'
 import mobileStyles from '../../styles/generator/canvas/mobile.module.css'
 import Image from 'next/image'
+import { useSpring, animated } from '@react-spring/web'
+import { useDrag } from '@use-gesture/react'
 
 type MyProps = {
     parent:any
@@ -14,6 +16,21 @@ type MyStates = {
 interface Canvas {
 parent: any
 }
+// onMouseDown={(e) => setMousedown(true)} onPointerDown={(e) => setMousedown(true)} onPointerUp={(e) => setMousedown(false)}
+const IMG = ()=> {
+  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
+  const bind = useDrag(({ down, movement: [mx, my] }) => {
+    api.start(i => {
+
+      return {x: mx * 10}
+
+
+    })
+  })
+  return <animated.div {...bind()} style={{ marginLeft:x }} className={mobileStyles.drag}>1111</animated.div>
+}
+
+
 
 class Canvas extends Component<MyProps, MyStates>
 {
@@ -30,12 +47,13 @@ class Canvas extends Component<MyProps, MyStates>
 
   render() 
   {
-
       
       return  <div className={this.parent.state.isMobile? mobileStyles.container : styles.container}>
         
                 <div className={this.parent.state.isMobile? mobileStyles.canvas : styles.canvas}>
-                  <img id='canvasIMG' className={this.parent.state.isMobile? mobileStyles.img : styles.img} src="/generator/will_smith_punching/raw.png" alt="meme" />
+                <IMG />
+                 {/*  <img id='canvasIMG' className={this.parent.state.isMobile? mobileStyles.img : styles.img} src="/generator/will_smith_punching/raw.png" alt="meme" />
+                   */}
                 </div>
               </div>
   }

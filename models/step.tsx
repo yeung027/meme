18e2b047e4ebda9eb5121f53631544921b20d606
yeung027 <shyeung_sfo@wfsfaa.gov.hk>
2022-07-13 11:34:02 +0800

    rewrite!!!!!!!!

--- models/step.tsx
+++
@@ -1,5 +0,0 @@
-export enum STEP {
-    UPLOADIMG = 'uploadimg',
-    EDITIMG = 'editimg',
-    EXPORT = 'export'
-  }
--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import 'boxicons/css/boxicons.min.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp

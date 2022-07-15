73a95361701cd0defc1a61bee98104db1abc4d85
yeung027 <shyeung_sfo@wfsfaa.gov.hk>
2022-07-15 13:59:04 +0800

    is dark mode detecter

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,30 +12,32 @@ import {
   generatorState,
   GeneratorState,
 } from '../reducers/generator';
+import { 
+  MemeState,
+  memeState as originMemeState,
+} from "../reducers/meme";
 
 const Home: NextPage = () => {
   const state:GeneratorState = useAppSelector(generatorState);
+  const memeState:MemeState = useAppSelector(originMemeState);
   const dispatch = useAppDispatch();
   const [mode, setMode] = useState('light');
   
   useEffect(() => {
     fontLoader();
 
-    window.matchMedia('(prefers-color-scheme: dark)')
-  .addEventListener('change', event => {
-      const colorScheme:string = event.matches ? "dark" : "light";
-      console.log(colorScheme); // "dark" or "light"
-      setMode(colorScheme);
- });
+   
 
     if(document && document.body)
       document.body.classList.add('fixed');
 
-      alert(mode);
+      //alert(mode);
   });
 //grid-rows-[70px_1fr_96px] border border-4 border-black 
+  let containerClass = 'desktop:bg-white w-screen h-screen grid grid-rows-[70px_1fr] desktop:flex desktop:flex-col overflow-hidden font-roboto';
+  containerClass += memeState.darkMode ? ' bg-black' : ' bg-my-purple'
   return (
-    <div className='bg-my-purple desktop:bg-white w-screen h-screen grid grid-rows-[70px_1fr] desktop:flex desktop:flex-col overflow-hidden font-roboto'>
+    <div className={containerClass}>
       <div className='w-full h-[70px] z-10'>
         <ExportDialog />
         <Menu />

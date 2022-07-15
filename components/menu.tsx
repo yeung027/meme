67ed555da7d7576a7c09c6eb8a8ac823f91e547a
yeung027 <shyeung_sfo@wfsfaa.gov.hk>
2022-07-15 15:31:40 +0800

    dark mode #2

--- components/menu.tsx
+++ components/menu.tsx
@@ -18,7 +18,7 @@ export default function Menu()
                 <div className="w-4/6 desktop:w-72 h-screen bg-lightGray dark:bg-my-darkGray shadow-[8px_0_10px_5px_rgba(0,0,0,0.2)]">
                     <div className="fixed w-4/6 desktop:w-72 flex flex-row justify-end pt-2 pr-2 z-30">
                         <BiX 
-                            className="text-my-purple2 dark:text-my-orange text-xl cursor-pointer"
+                            className="text-my-purple3 dark:text-my-orange text-xl cursor-pointer"
                             onClick={() => dispatch(menuOpen(false))}
                          />
                     </div>
@@ -29,7 +29,7 @@ export default function Menu()
                         </li>
                     </ul>
                 </div>
-                <div className="w-2/6 h-screen" onClick={() => dispatch(menuOpen(false))} />
+                <div className="w-2/6 desktop:w-[calc(100%-288px)] h-screen" onClick={() => dispatch(menuOpen(false))} />
                 
             </div>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -37,12 +37,12 @@ const Home: NextPage = () => {
         <Header />
       </div>
       <div className='grid grid-rows-[1fr_96px] desktop:flex desktop:flex-row-reverse desktop:gap-6 desktop:h-full desktop:bg-lightGray'>
-        <div className='hidden desktop:block absolute bg-my-dark w-6 h-full left-0 -z-10 dark:z-30 left-[238px]' />
+        <div className='hidden desktop:block absolute bg-my-dark w-6 h-full left-0 -z-10 dark:z-0 left-[238px]' />
         <div className='bg-lightGray dark:bg-my-dark w-full grid grid-rows-[48px_1fr] justify-items-center gap-y-4 rounded-t-3xl desktop:rounded-none p-6 shadow-[0_-10px_7px_2px_rgba(105,0,197,1)] dark:shadow-[0_-10px_7px_2px_rgba(23,23,23,0.5)] desktop:shadow-none z-0'>
           <Steps />
           <Canvas />
         </div>
-        <div className='border-t desktop:border-0 border-black dark:border-my-dark h-24 w-full bg-white dark:bg-my-darkGray desktop:h-full desktop:w-[300px] desktop:shadow-[1px_0_15px_1px_rgba(0,0,0,0.2)]'>
+        <div className='border-t desktop:border-0 border-my-gray2 dark:border-my-dark h-24 w-full bg-white dark:bg-my-darkGray desktop:h-full desktop:w-[300px] desktop:shadow-[1px_0_15px_1px_rgba(0,0,0,0.2)]'>
           <ControlPanel />
         </div>
       </div>

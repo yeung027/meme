ce96205d3045c598f488f8050f401504f2ebffdc
711 <rm711@711deMac-mini.local>
2022-07-15 23:37:34 +0800

    darkmode with tailwind and js code is more better ~ :)

--- helpers/common.ts
+++ helpers/common.ts
@@ -1,4 +1,27 @@
+import { ThunkDispatch } from "@reduxjs/toolkit";
 import { useAppDispatch } from "../app/hooks";
+import { 
+    setDarkMode,
+} from "../reducers/meme";
+
 export const delay = (ms:number) => new Promise(res => setTimeout(res, ms));
 
 export const darkModeTransformClass:string = ' delay-1000 duration-1000 ease-in-out transform';
+
+export const addDarkModeListener = async (dispatch:any) => {
+    let retryCount:number = 0;
+    while(!window)
+    {
+        await delay(70);
+        retryCount++;
+        if(retryCount>30) return false;
+    }
+    
+    dispatch(setDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches));
+    window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', event => {
+        dispatch(setDarkMode(event.matches))
+    });
+
+    return true;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import ExportDialog from '../components/generator/exportDialog';
 import Steps from '../components/generator/steps';
 import Header from '../components/header';
 import Menu from '../components/menu';
-import { darkModeTransformClass } from '../helpers/common';
+import { addDarkModeListener, darkModeTransformClass } from '../helpers/common';
 import {
   generatorState,
   GeneratorState,
@@ -28,26 +28,39 @@ const Home: NextPage = () => {
 
     if(document && document.body)
       document.body.classList.add('fixed');
+    addDarkModeListener(dispatch);
   });
 
+  let containerClass = 'bg-my-purple dark:bg-dark-purple w-screen h-screen grid grid-rows-[70px_1fr] desktop:flex desktop:flex-col overflow-hidden font-roboto'+darkModeTransformClass;
+  let row2ContainerClass = 'grid grid-rows-[1fr_96px] desktop:flex desktop:flex-row-reverse desktop:gap-6 desktop:h-full';
+  if(memeState.darkMode)
+  {
+    row2ContainerClass += ' desktop:bg-my-dark';
+  }
+  else
+  {
+    containerClass += ' desktop:bg-white';
+    row2ContainerClass += ' desktop:bg-lightGray ';
+  }
+
   return (
-    <div className={'bg-my-purple desktop:bg-white dark:bg-dark-purple w-screen h-screen grid grid-rows-[70px_1fr] desktop:flex desktop:flex-col overflow-hidden font-roboto'+darkModeTransformClass}>
+    <div className={containerClass}>
       <div className='w-full h-[70px] z-10'>
         <ExportDialog />
         <Menu />
         <Header />
       </div>
-      <div className='grid grid-rows-[1fr_96px] desktop:flex desktop:flex-row-reverse desktop:gap-6 desktop:h-full desktop:bg-lightGray'>
-        <div className='mobile:hidden desktop:-left-[238px] absolute bg-my-dark w-6 h-full left-0 z-10 border border-red-500 left-[238px]' />
+      <div className={row2ContainerClass}>
         <div className={'bg-lightGray dark:bg-my-dark w-full grid grid-rows-[48px_1fr] justify-items-center gap-y-4 rounded-t-3xl desktop:rounded-none p-6 shadow-[0_-10px_7px_2px_rgba(105,0,197,1)] dark:shadow-[0_-10px_7px_2px_rgba(23,23,23,0.5)] desktop:shadow-none z-0'+darkModeTransformClass}>
           <Steps />
           <Canvas />
         </div>
-        <div className={'border-t desktop:border-0 border-my-gray2 dark:border-my-dark h-24 w-full bg-white dark:bg-my-darkGray desktop:h-full desktop:w-[300px] desktop:shadow-[1px_0_15px_1px_rgba(0,0,0,0.2)]'+darkModeTransformClass}>
+        <div className={'border-t desktop:border-0 border-my-gray2 dark:border-my-dark h-24 w-full bg-white dark:bg-my-darkGray2 desktop:h-full desktop:w-[300px] desktop:shadow-[1px_0_15px_1px_rgba(0,0,0,0.2)]'+darkModeTransformClass}>
           <ControlPanel />
         </div>
       </div>
     </div>
+    //
   )
 }
 

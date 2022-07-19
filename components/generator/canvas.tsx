e845cf2ff2acda316f2cfe720304147f4d5c1626
yeung027 <shyeung_sfo@wfsfaa.gov.hk>
2022-07-19 10:10:48 +0800

    commit for start dev landscape: dev, lol

--- components/generator/canvas.tsx
+++ components/generator/canvas.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { wideEnoughToSetLandscape } from "../../helpers/generator/image";
 import { GeneratorState } from "../../reducers/generator";
@@ -13,26 +13,29 @@ import {
 export default function Canvas()
 {
     const [rotateCanvas, setRotateCanvas] = useState(false);
+    const containerEl   = useRef(null);
+    const imgEl         = useRef(null);
 
     useEffect(() => {
         wideEnoughToSetLandscape(generatorState.rawImageUrl).then((result:boolean)=>{
-            console.log('result: '+result);
             setRotateCanvas(result);
+            //let containerDivEl:HTMLDivElement   = containerEl.current!;
+            //let htmlImgEl:HTMLImageElement      = imgEl.current!;
+            //rotateImgToLandscape(containerDivEl, htmlImgEl);
         })
     });
 
     const memeState:MemeState = useAppSelector(originMemeState);
     const generatorState:GeneratorState = useAppSelector(originGeneratorState);
     const dispatch = useAppDispatch();
-    let containerClass = 'flex flex-col justify-center items-center';
+    let containerClass = 'flex flex-col justify-center items-center border border-2 border-red-500';
     containerClass += ' w-full h-[calc(100vh-70px-96px-48px-16px-24px-24px)] desktop:h-[calc(100vh-70px-24px-24px-16px-48px)]';
 
-    let imageClass = 'border-2 border-my-purple4';
-    if(rotateCanvas) imageClass += ' -rotate-90 desktop:rotate-0 ';
-    else imageClass += ' max-w-full max-h-full';
+    let imageClass = 'border-2 border-my-purple4 max-w-full max-h-full';
 
-    return  <div className={containerClass}>
+    return  <div className={containerClass} ref={containerEl}>
                 <img 
+                    ref={imgEl}
                     className={imageClass}
                     src={generatorState.rawImageUrl}
                  />

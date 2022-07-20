7cfe5ed1b826dae968eccd2cf1b8e382a4f4ff1e
yeung027 <shyeung_sfo@wfsfaa.gov.hk>
2022-07-20 09:42:42 +0800

    landscape #10

--- components/header.tsx
+++ components/header.tsx
@@ -25,8 +25,8 @@ export default function Header(props:{title:string})
         if(isMobile)
         {
             setContainerClass(defaultContainerClass+' landscape:h-[-webkit-fill-available] landscape:flex landscape:flex-col-reverse landscape:justify-start');
-            setSubContainer1Class(defaultSubcontainer1Class+' landscapeText landscape:w-[70px] landscape:pl-0 landscape:pl-6');
-            setSubContainer2Class(defaultSubcontainer2Class+' landscape:w-[70px] landscape:pr-0 landscape:items-center landscape:flex-col landscape:pt-2 landscape:top-0');
+            setSubContainer1Class(defaultSubcontainer1Class+' landscapeText landscape:w-[70px] landscape:pl-0 landscape:pl-6 landscape:items-center landscape:justify-end');
+            setSubContainer2Class(defaultSubcontainer2Class+' landscape:w-[70px] landscape:pr-0 landscape:items-center landscape:justify-start landscape:flex-col landscape:pt-2 landscape:top-0');
         }
     });
 

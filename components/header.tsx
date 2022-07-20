51d73cf6e43b626c045bd70388ad899af1d41c43
yeung027 <shyeung_sfo@wfsfaa.gov.hk>
2022-07-20 10:27:19 +0800

    landscape #17

--- components/header.tsx
+++ components/header.tsx
@@ -25,8 +25,8 @@ export default function Header(props:{title:string})
         if(isMobile)
         {
             setContainerClass(defaultContainerClass+' landscape:h-[-webkit-fill-available] landscape:grid landscape:grid-rows-2 landscape:justify-end');
-            setSubContainer1Class(defaultSubcontainer1Class+' border border-4 border-blue-500 landscape:leading-[70px] landscape:self-center landscape:rotate-180 landscapeText landscape:w-[70px] landscape:pl-0 landscape:pl-6 landscape:items-center landscape:justify-end');
-            setSubContainer2Class(defaultSubcontainer2Class+' border border-4 border-red-500 landscape:w-[70px] landscape:pr-0 landscape:items-center landscape:justify-end landscape:flex-col landscape:pt-2 landscape:top-0');
+            setSubContainer1Class(defaultSubcontainer1Class+' landscape:leading-[70px] landscape:rotate-180 landscapeText landscape:w-[70px] landscape:pl-0 landscape:justify-end landscape:pt-2');
+            setSubContainer2Class(defaultSubcontainer2Class+' landscape:w-[70px] landscape:pr-0 landscape:items-center landscape:justify-end landscape:flex-col landscape:pb-2');
         }
     });
 

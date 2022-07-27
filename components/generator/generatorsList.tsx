5654990345af30d4db168df07794c7a3b22b8e7c
yeung027 <shyeung_sfo@wfsfaa.gov.hk>
2022-07-27 09:28:02 +0800

    index scroll y if overflow

--- components/generator/generatorsList.tsx
+++ components/generator/generatorsList.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
 import { BiX, BiHomeAlt } from "react-icons/bi";
+import { darkModeTransformClass } from "../../helpers/common";
 
 export default function GeneratorList()
 {
 
 
-    let componentClass:string = "h-full flex flex-col gap-4 min-w-[80%]";
-    let itemClass:string = "rounded-3xl bg-white dark:bg-my-gray py-1.5 px-3.5 shadow-[5px_5px_15px_3px_rgba(0,0,0,0.09)] flex flex-row";
-    let avatorClass:string = "rounded-full border-2 border-my-purple2 dark:border-dark-purple flex justify-center content-center w-[51px] h-[51px] overflow-hidden";
+    let componentClass:string = "overflow-hidden h-fit flex flex-col gap-4 min-w-[80%]";
+    let itemClass:string = "rounded-2xl bg-white dark:bg-my-gray py-1.5 px-3.5 shadow-[5px_5px_15px_3px_rgba(0,0,0,0.09)] flex flex-row"+darkModeTransformClass;
+    let avatorClass:string = "rounded-full border-2 border-my-purple2 dark:border-dark-purple flex justify-center content-center w-[51px] h-[51px] overflow-hidden"+darkModeTransformClass;
     let avatorImgClass:string = "w-[46px] h-[46px] m-auto";
     let listItemCenterClass:string = "flex flex-col justify-center content-center ml-5 gap-px";
-    let span1Class:string = "h-5 text-black dark:text-my-yellow2 text-base font-semibold";
-    let span2Class:string = "h-5 text-lightBlue dark:text-my-yellow text-sm font-normal";
+    let span1Class:string = "h-5 text-black dark:text-my-yellow2 text-base font-semibold"+darkModeTransformClass;
+    let span2Class:string = "h-5 text-lightBlue dark:text-my-yellow text-sm font-normal"+darkModeTransformClass;
 
     return  <div className={componentClass}>
                 <Link href="/example">
@@ -29,7 +30,7 @@ export default function GeneratorList()
                 <Link href="/">
                     <a className={itemClass}>
                         <div className={avatorClass}>
-                            <img className={avatorImgClass} src="generator/example/p1.jpg" />
+                            <img className={avatorImgClass} src="generator/example/2.jpg" />
                         </div>
                         <div className={listItemCenterClass}>
                             <span className={span1Class}>hello</span>
@@ -37,6 +38,126 @@ export default function GeneratorList()
                         </div>
                     </a>
                 </Link>
-                
+
+                <Link href="/">
+                    <a className={itemClass}>
+                        <div className={avatorClass}>
+                            <img className={avatorImgClass} src="generator/example/2.jpg" />
+                        </div>
+                        <div className={listItemCenterClass}>
+                            <span className={span1Class}>hello</span>
+                            <span className={span2Class}>bye</span>
+                        </div>
+                    </a>
+                </Link>
+
+                <Link href="/">
+                    <a className={itemClass}>
+                        <div className={avatorClass}>
+                            <img className={avatorImgClass} src="generator/example/2.jpg" />
+                        </div>
+                        <div className={listItemCenterClass}>
+                            <span className={span1Class}>hello</span>
+                            <span className={span2Class}>bye</span>
+                        </div>
+                    </a>
+                </Link>
+
+                <Link href="/">
+                    <a className={itemClass}>
+                        <div className={avatorClass}>
+                            <img className={avatorImgClass} src="generator/example/2.jpg" />
+                        </div>
+                        <div className={listItemCenterClass}>
+                            <span className={span1Class}>hello</span>
+                            <span className={span2Class}>bye</span>
+                        </div>
+                    </a>
+                </Link>
+
+                <Link href="/">
+                    <a className={itemClass}>
+                        <div className={avatorClass}>
+                            <img className={avatorImgClass} src="generator/example/2.jpg" />
+                        </div>
+                        <div className={listItemCenterClass}>
+                            <span className={span1Class}>hello</span>
+                            <span className={span2Class}>bye</span>
+                        </div>
+                    </a>
+                </Link>
+
+                <Link href="/">
+                    <a className={itemClass}>
+                        <div className={avatorClass}>
+                            <img className={avatorImgClass} src="generator/example/2.jpg" />
+                        </div>
+                        <div className={listItemCenterClass}>
+                            <span className={span1Class}>hello</span>
+                            <span className={span2Class}>bye</span>
+                        </div>
+                    </a>
+                </Link>
+
+                <Link href="/">
+                    <a className={itemClass}>
+                        <div className={avatorClass}>
+                            <img className={avatorImgClass} src="generator/example/2.jpg" />
+                        </div>
+                        <div className={listItemCenterClass}>
+                            <span className={span1Class}>hello</span>
+                            <span className={span2Class}>bye</span>
+                        </div>
+                    </a>
+                </Link>
+
+                <Link href="/">
+                    <a className={itemClass}>
+                        <div className={avatorClass}>
+                            <img className={avatorImgClass} src="generator/example/2.jpg" />
+                        </div>
+                        <div className={listItemCenterClass}>
+                            <span className={span1Class}>hello</span>
+                            <span className={span2Class}>bye</span>
+                        </div>
+                    </a>
+                </Link>
+
+                <Link href="/">
+                    <a className={itemClass}>
+                        <div className={avatorClass}>
+                            <img className={avatorImgClass} src="generator/example/2.jpg" />
+                        </div>
+                        <div className={listItemCenterClass}>
+                            <span className={span1Class}>hello</span>
+                            <span className={span2Class}>bye</span>
+                        </div>
+                    </a>
+                </Link>
+
+                <Link href="/">
+                    <a className={itemClass}>
+                        <div className={avatorClass}>
+                            <img className={avatorImgClass} src="generator/example/2.jpg" />
+                        </div>
+                        <div className={listItemCenterClass}>
+                            <span className={span1Class}>hello</span>
+                            <span className={span2Class}>bye</span>
+                        </div>
+                    </a>
+                </Link>
+
+                <Link href="/">
+                    <a className={itemClass}>
+                        <div className={avatorClass}>
+                            <img className={avatorImgClass} src="generator/example/2.jpg" />
+                        </div>
+                        <div className={listItemCenterClass}>
+                            <span className={span1Class}>hello</span>
+                            <span className={span2Class}>3</span>
+                        </div>
+                    </a>
+                </Link>
+                 
             </div>
 }

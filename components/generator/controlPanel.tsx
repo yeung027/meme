0da562e57cf361fd05ce6db0c4eb94d4f65d4cc1
yeung027 <shyeung_sfo@wfsfaa.gov.hk>
2022-07-28 10:04:56 +0800

    test control panel ui ease with hardcode delay out and in

--- components/generator/controlPanel.tsx
+++ components/generator/controlPanel.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { delay } from "../../helpers/common";
 import { 
+    controlPanelEase,
     MemeState, 
     memeState as originMemeState,
 } from "../../reducers/meme";
@@ -10,6 +13,17 @@ export default function ControlPanel()
     const memeState:MemeState = useAppSelector(originMemeState);
     const dispatch = useAppDispatch();
 
+    const show = async() =>{
+        await delay(2000);
+        dispatch(controlPanelEase(false));
+        await delay(2000);
+        dispatch(controlPanelEase(true));
+    }
+
+    useEffect(() => {
+        show();
+    });
+      
     let componentClass:string = "w-full h-full flex justify-center items-center duration-500 ease-in-out transform";
     componentClass += memeState.controlPanelEase ? " translate-y-0" : " translate-y-full";
     
